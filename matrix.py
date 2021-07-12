4698f6266eeb5e69c1fb68e182586e8275cd46bd
jihongeek <jihongeek@kakao.com>
2021-07-13 00:14:19 +0900

    fixed inverse matrix func because of index error

--- matrix.py
+++ matrix.py
@@ -41,7 +41,7 @@ def scalarMultiply(scalar, matrix):
 
 def inverse(matrix : list):
     cofactorMatrix = [[0]*len(matrix) for _ in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            cofactorMatrix[i][j] = determinant.cofactor(matrix,i,j)
-    return scalarMultiply(determinant.det(matrix), transpose(cofactorMatrix))
+    for i in range(1,len(matrix)+1):
+        for j in range(1,len(matrix)+1):
+            cofactorMatrix[i-1][j-1] = determinant.cofactor(matrix,i,j)
+    return scalarMultiply(1/determinant.det(matrix), transpose(cofactorMatrix))
